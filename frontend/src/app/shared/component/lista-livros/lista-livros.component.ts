239ef87e8180df0deafb18112028e5421f778c15
Wellington Oliveira dos Santos <wellington.oliveira.dev@gmail.com>
2022-11-27 16:29:42 -0300

    Ajustando o layout da tela home

--- frontend/src/app/shared/component/lista-livros/lista-livros.component.ts
+++ frontend/src/app/shared/component/lista-livros/lista-livros.component.ts
@@ -3,29 +3,31 @@ import {Livro} from "../../../model/livro.model";
 import {UserService} from "../../../_services/user.service";
 
 @Component({
-  selector: 'lista-livros',
-  templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+    selector: 'lista-livros',
+    templateUrl: './lista-livros.component.html',
+    styleUrls: ['./lista-livros.component.css']
 })
 export class ListaLivrosComponent implements OnChanges {
-  @Input() livros: Livro[] = [];
-  dataSource: Livro[] = [];
-  constructor(private userService: UserService) { }
+    @Input() livros: Livro[] = [];
+    @Input('cols') cols: string = "2";
+    dataSource: Livro[] = [];
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.dataSource = changes['livros'].currentValue;
-  }
-
-  marcarLivroLido(evento: any, id: number) {
-    if(evento.checked) {
-      this.userService.marcarLivroLido(id).subscribe(
-        { next: () => console.log('retorno marcar livro') }
-      );
-    } else {
-      this.userService.desmarcarLivroLido(id).subscribe(
-        { next: () => console.log('retorno desmarcar livro') }
-      );
+    constructor(private userService: UserService) {
     }
-  }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        this.dataSource = changes['livros'].currentValue;
+    }
+
+    marcarLivroLido(evento: any, id: number) {
+        if (evento.checked) {
+            this.userService.marcarLivroLido(id).subscribe(
+                {next: () => console.log('retorno marcar livro')}
+            );
+        } else {
+            this.userService.desmarcarLivroLido(id).subscribe(
+                {next: () => console.log('retorno desmarcar livro')}
+            );
+        }
+    }
 }
